ec5e923be14e1f8d941e7401d9169a25bfdffd4b
Pramod <pramodmahajan1411200@gmail.com>
2022-07-02 07:13:36 +0530

    change welcome api

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 const dotenv = require("dotenv");
-dotenv.config({path:'./config.env'});
-const express = require('express');
+dotenv.config({ path: "./config.env" });
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const cookiesparser = require('cookie-parser');
-const fileUpload = require('express-fileupload');
-const PORT = process.env.PORT || 5000
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookiesparser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(bodyParser.json());
 // connect to mongodb database
-require('./DB/dataconnection');
+require("./DB/dataconnection");
 
 app.use(cookiesparser());
 
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); 
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 
-app.use(fileUpload({
-    useTempFiles:true
-}));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
 //Routrs
-app.use('/user',require('./routes/rounting'))
-app.use('/api',require('./routes/upload'))
+app.use("/user", require("./routes/rounting"));
+app.use("/api", require("./routes/upload"));
 
 // app.use(express.urlencoded({extended:false}));
 
+app.get("/", (req, res) => {
+  res.send("Welcome Bluerocket");
+});
 
-
-
-app.get("/",(req,res)=>{
-    res.send("hello pramod mahajan");
-})
-
-app.listen(PORT,()=>{
-    console.log("Running server on PORT ",PORT );
-})
-    
+app.listen(PORT, () => {
+  console.log("Running server on PORT ", PORT);
+});
